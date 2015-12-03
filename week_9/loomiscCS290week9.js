e1fdc3ef8c5c752706c6d9ae8381026ceaa207fd
chrisloomis13 <christopherfrenchloomis@gmail.com>
2015-12-02 17:42:22 -0800

    240-week9-db

--- week_9/loomiscCS290week9.js
+++ week_9/loomiscCS290week9.js
@@ -17,12 +17,107 @@ var mysql = require('./mysqlpool.js');
 
 app.get('/',function(req,res,next){
 	var context = {};
-	mysql.pool.query('CREATE TABLE IF NOT EXISTS todo(id INT PRIMARY KEY AUTO_INCREMENT,name VARCHAR(255) NOT NULL,done BOOLEAN,due DATE)', function(err, rows, fields){
+//create table if it does not exist
+	mysql.pool.query("CREATE TABLE IF NOT EXISTS workouts("+
+    "id INT PRIMARY KEY AUTO_INCREMENT,"+
+    "name VARCHAR(255) NOT NULL,"+
+    "reps INT,"+
+    "weight INT,"+
+    "date DATE,"+
+    "lbs BOOLEAN)", function(err, rows, fields){
     	if(err){
 			next(err);
 			return;
 		}});
-	mysql.pool.query('SELECT * FROM todo', function(err, rows, fields){
+	
+	function validateDate() {
+    	if(document.forms[addExercise][month].value != null || document.forms[addExercise][month].value != ""){
+			if(document.forms[addExercise][day].value != null || document.forms[addExercise][day].value != ""){
+				if(document.forms[addExercise][year].value != null || document.forms[addExercise][year].value != ""){
+					if(!(document.forms[addExercise][year].value.length > 5)){
+						return true;
+					}
+				}
+			}
+		}
+		else if(document.forms[addExercise][month].value == null || document.forms[addExercise][month].value == ""){
+			if(document.forms[addExercise][day].value == null || document.forms[addExercise][day].value == ""){
+				if(document.forms[addExercise][year].value == null || document.forms[addExercise][year].value == ""){
+					return true;
+				}
+			}
+		}
+		else{
+			return false;
+		}
+	}
+	
+//post listener for adding row
+	document.getElementById("newExerciseSubmit").addEventListener("click", function(event){
+		var req = new XMLHttpRequest();
+		var payload = {exerciseName: null,
+					  reps: null,
+					  weight: null,
+					  date: null,
+					  lbs: null};
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////		
+		payload.name = document.getElementById("exerciseName").value;
+		
+		if(document.forms[addExercise][reps].value != null || document.forms[addExercise][reps].value != ""){
+			payload.reps = document.getElementById("reps").value; 
+		}
+		
+		if(document.forms[addExercise][weight].value != null || document.forms[addExercise][weight].value != ""){
+			payload.weight = document.getElementById("weight").value; 
+		}
+		
+		if(document.forms[addExercise][month].value != null || document.forms[addExercise][month].value != ""){
+			if(document.forms[addExercise][day].value != null || document.forms[addExercise][day].value != ""){
+				if(document.forms[addExercise][year].value != null || document.forms[addExercise][year].value != ""){
+					if(!(document.forms[addExercise][year].value.length > 5)){
+						payload.date = document.getElementById("year").value+"-"+document.getElementById("month").value+"-"+document.getElementById("day").value;
+					}
+				}
+			}
+		}
+		else(document.forms[addExercise][month].value == null || document.forms[addExercise][month].value == ""){
+			if(document.forms[addExercise][day].value == null || document.forms[addExercise][day].value == ""){
+				if(document.forms[addExercise][year].value == null || document.forms[addExercise][year].value == ""){
+					payload.date = null;
+				}
+			}
+		}
+		
+		if(document.forms[addExercise][month].value == "lbs"){
+			payload.lbs = 1;
+		}
+		
+		if(document.forms[addExercise][month].value == "kg"){
+			payload.lbs = 0;
+		}
+																					/////////////////UPDATE
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////			
+		req.open("POST", "/insert", true);
+		req.setRequestHeader('Content-Type', 'application/json');
+		req.addEventListener("load",function(){
+			if(req.status >= 200 && req.status < 400){//no error so utilize data returned
+				//var response = JSON.parse(req.responseText);
+				//we need to parse the JSON inside the JSON returned
+				//response = JSON.parse(response.data);
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////					
+				//document.getElementById("response").textContent = response.input; 				/////////////////UPDATE
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////					
+			}
+			else{//error
+				console.log("Error in network request: " + request.statusText);
+			}
+		});
+		req.send(JSON.stringify(payload));
+		event.preventDefault();
+	});
+	
+//get table	
+	mysql.pool.query('SELECT * FROM workouts', function(err, rows, fields){
 		if(err){
 			next(err);
 			return;
@@ -35,11 +130,13 @@ app.get('/',function(req,res,next){
 app.get('/reset-table',function(req,res,next){
   var context = {};
   mysql.pool.query("DROP TABLE IF EXISTS todo", function(err){
-    var createString = "CREATE TABLE todo(" +
-    "id INT PRIMARY KEY AUTO_INCREMENT," +
-    "name VARCHAR(255) NOT NULL," +
-    "done BOOLEAN," +
-    "due DATE)";
+    var createString = "CREATE TABLE workouts("+
+    "id INT PRIMARY KEY AUTO_INCREMENT,"+
+    "name VARCHAR(255) NOT NULL,"+
+    "reps INT,"+
+    "weight INT,"+
+    "date DATE,"+
+    "lbs BOOLEAN)";
     mysql.pool.query(createString, function(err){
       context.results = "Table reset";
       res.render('home',context);
@@ -49,7 +146,7 @@ app.get('/reset-table',function(req,res,next){
 
 app.get('/insert',function(req,res,next){
   var context = {};
-  mysql.pool.query("INSERT INTO todo (`name`) VALUES (?)", [req.query.c], function(err, result){
+  mysql.pool.query("INSERT INTO workouts (`name`,`reps`,`weight`,`date`,`lbs`) VALUES (?,?,?,?,?)", [req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs], function(err, result){
     if(err){
       next(err);
       return;
