260d00c90b5822991c2961c83fd49214ff1d097b
chrisloomis13 <christopherfrenchloomis@gmail.com>
2015-12-02 19:04:30 -0800

    240

--- week_9/loomiscCS290week9.js
+++ week_9/loomiscCS290week9.js
@@ -63,7 +63,7 @@ app.get('/reset-table',function(req,res,next){
 
 app.post('/insert',function(req,res,next){
   var context = {};
-  mysql.pool.query("INSERT INTO workouts (`name`,`reps`,`weight`,`date`,`lbs`) VALUES (?,?,?,?,?)", [req.query.exerciseName, req.query.reps, req.query.weight, req.query.date, req.query.lbs], function(err, result){
+  mysql.pool.query("INSERT INTO workouts (`name`,`reps`,`weight`,`date`,`lbs`) VALUES (?,?,?,?,?)", [req.body.exerciseName, req.body.reps, req.body.weight, req.body.date, req.body.lbs], function(err, result){
     if(err){
       next(err);
       return;
